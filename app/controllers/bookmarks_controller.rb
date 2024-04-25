aea882984412af8648233deebe823ca2f5d6aab1
amy hastings <amyh364@outlook.com>
2024-04-25 15:23:53 +0100

    challenge2 backend

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,35 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [:destroy]
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to lists_path, notice: "bookmark added"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+   def destroy
+    # @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to bookmark_path, notice: 'Bookmark deleted successfully.'
+  end
+
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
