c136c253f478e419534223eaaff077ae1052a518
amy hastings <amyh364@outlook.com>
2024-04-26 17:55:40 +0100

    movie api start

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem "stimulus-rails"
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
-
+gem 'rest-client'
 #front-end gems
 gem "bootstrap", "~> 5.2"
 gem "autoprefixer-rails"
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,33 @@ Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conf
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+require 'rest-client'
+
+
+puts 'getting movie datta'
+# def api_key
+#   ENV['api_key']
+# end
+def movie_dataset
+  # api_data = {key: api_key}
+  # base_url = RestClient.get("https://tmdb.lewagon.com/configuration").then { |response| JSON.parse(response)["images"]["base_url"] }
+  # file_size = "w500"
+  movies = RestClient.get("https://tmdb.lewagon.com/movie/top_rated")
+  movies_array = JSON.parse(movies)['results']
+  movies_array.each do |m|
+    poster_path = m["poster_path"]
+    if poster_path
+      full_poster_url = "https://image.tmdb.org/t/p/w500/#{poster_path}"
+    else
+      full_poster_url = nil
+    end
+    Movie.create(
+      title: m["title"],
+      overview: m["overview"],
+      poster_url: full_poster_url,
+      rating: m["vote_average"]
+    )
+  end
+end
+movie_dataset()
+puts "seeding movies data"
